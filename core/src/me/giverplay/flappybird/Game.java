29ddc46b74979c4378af10e5635496623bd49cdd
GiverPlay007 <play.giver.007@gmail.com>
2021-11-15 20:23:28 -0300

    Carregando imagens

--- core/src/me/giverplay/flappybird/Game.java
+++ core/src/me/giverplay/flappybird/Game.java
@@ -1,31 +1,30 @@
 package me.giverplay.flappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Game extends ApplicationAdapter {
-  SpriteBatch batch;
-  Texture img;
+
+  private SpriteBatch batch;
+  private Texture passaro;
 
   @Override
   public void create() {
     batch = new SpriteBatch();
-    img = new Texture("badlogic.jpg");
+    passaro = new Texture("passaro1.png");
   }
 
   @Override
   public void render() {
-    ScreenUtils.clear(1, 0, 0, 1);
     batch.begin();
-    batch.draw(img, 0, 0);
+    batch.draw(passaro, 100f, 100f);
     batch.end();
   }
 
   @Override
   public void dispose() {
     batch.dispose();
-    img.dispose();
   }
 }
